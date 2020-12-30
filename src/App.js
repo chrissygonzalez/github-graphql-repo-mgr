f06e94d141eea43d6ac36bf9889b92779557742f
Chrissy Gonzalez <chrissy.gonzalez@electric.ai>
2020-12-30 11:30:40 -0500

    add a little styling

--- src/App.js
+++ src/App.js
@@ -3,56 +3,8 @@ import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import './App.css';
 
-const GET_USERS = gql`
-  {
-    users {
-      id
-      login
-      avatar_url
-    }
-  }
-`;
-
-const SEARCH_FOR_REPOS = gql`
-  query($search_term: String!) {
-    search(query: $search_term, type: REPOSITORY, first: 20) {
-      repositoryCount
-      edges {
-        node {
-          ... on Repository {
-            name
-            owner {
-              login
-            }
-            stargazers {
-              totalCount
-            }
-            descriptionHTML
-          }
-        }
-      }
-    }
-  }
-`;
-
-const USER_FORKED_REPOS = gql`
-  query USER_FORKED_REPOS($login: String!) {
-    user(login: $login) {
-      repositories(last: 100) {
-        edges {
-          node {
-            id
-            name
-            isFork
-          }
-        }
-      }
-    }
-  }
-`;
-
-const TRY_THIS_QUERY = gql`
-  query TRY_THIS_QUERY($login: String!) {
+const FORKED_REPOS = gql`
+  query FORKED_REPOS($login: String!) {
     user(login: $login) {
       name
       websiteUrl
@@ -62,11 +14,14 @@ const TRY_THIS_QUERY = gql`
           node {
             nameWithOwner
             isFork
+            url
+            updatedAt
             parent {
               nameWithOwner
               owner {
                 login
               }
+              url
             }
           }
         }
@@ -75,8 +30,26 @@ const TRY_THIS_QUERY = gql`
   }
 `;
 
+const Repo = ({ repo }) => {
+  const lastUpdated = new Date(repo.node.updatedAt).toDateString();
+  return (
+    <li key={repo.cursor}>
+      <a href={repo.node.url} className="bold">
+        {repo.node.nameWithOwner}
+      </a>
+      <p className="small">Last updated {lastUpdated}</p>
+      <p className="small">
+        Forked from{' '}
+        <a href={repo.node.parent.url}>{repo.node.parent.nameWithOwner}</a> by{' '}
+        {repo.node.parent.owner.login}
+      </p>
+      <button onClick={() => alert('deleting!')}>Delete forked repo</button>
+    </li>
+  );
+};
+
 function App() {
-  const { loading, error, data } = useQuery(TRY_THIS_QUERY, {
+  const { loading, error, data } = useQuery(FORKED_REPOS, {
     variables: { login: 'chrissygonzalez' },
   });
   if (error) return { error };
@@ -87,12 +60,7 @@ function App() {
     <main className="App">
       <ul>
         {forkedRepos.map((repo) => {
-          return (
-            <li key={repo.cursor}>
-              {repo.node.nameWithOwner} forked from{' '}
-              {repo.node.parent.nameWithOwner} by {repo.node.parent.owner.login}
-            </li>
-          );
+          return <Repo repo={repo} />;
         })}
       </ul>
     </main>
