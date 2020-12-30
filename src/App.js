b6c409ade7cebeeee4b4654bf88d33436a2bdb03
Chrissy Gonzalez <chrissy.gonzalez@electric.ai>
2020-12-30 10:24:44 -0500

    query ten forked repos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 import './App.css';
 
+const GET_USERS = gql`
+  {
+    users {
+      id
+      login
+      avatar_url
+    }
+  }
+`;
+
+const SEARCH_FOR_REPOS = gql`
+  query($search_term: String!) {
+    search(query: $search_term, type: REPOSITORY, first: 20) {
+      repositoryCount
+      edges {
+        node {
+          ... on Repository {
+            name
+            owner {
+              login
+            }
+            stargazers {
+              totalCount
+            }
+            descriptionHTML
+          }
+        }
+      }
+    }
+  }
+`;
+
+const USER_FORKED_REPOS = gql`
+  query USER_FORKED_REPOS($login: String!) {
+    user(login: $login) {
+      repositories(last: 100) {
+        edges {
+          node {
+            id
+            name
+            isFork
+          }
+        }
+      }
+    }
+  }
+`;
+
+const TRY_THIS_QUERY = gql`
+  query TRY_THIS_QUERY($login: String!) {
+    user(login: $login) {
+      name
+      websiteUrl
+      repositories(first: 10, orderBy: { field: NAME, direction: DESC }) {
+        edges {
+          cursor
+          node {
+            nameWithOwner
+            isFork
+            parent {
+              nameWithOwner
+              owner {
+                login
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 function App() {
+  const { loading, error, data } = useQuery(TRY_THIS_QUERY, {
+    variables: { login: 'chrissygonzalez' },
+  });
+  if (error) return { error };
+  if (loading) return <h1>Loading...</h1>;
+  const forkedRepos = data.user.repositories.edges;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <ul>
+        {forkedRepos.map((repo) => {
+          return (
+            <li key={repo.cursor}>
+              {repo.node.nameWithOwner} forked from{' '}
+              {repo.node.parent.nameWithOwner} by {repo.node.parent.owner.login}
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const token = process.env.REACT_APP_GITHUB_KEY;
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
